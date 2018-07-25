8de9279603fe44a85519913fa9e151c09d4e10a5
RikhartBekkevold <rikhart.bekkevold@hig.no>
2018-07-25 15:14:59 +0200

    restructure

--- main.js
+++ main.js
@@ -4,35 +4,33 @@ var contact = document.getElementById('contact');
 
 home.style.display 				= 'none';
 contact.style.display 			= 'none';
-projectbtn.style.borderBottom 	= '1px solid #56535D';
+project_btn.style.borderBottom 	= '1px solid #56535D';
 
-projectbtn.addEventListener('pointerdown', function(event) {
-    // console.table(event);
+project_btn.addEventListener('pointerdown', function() {
     project.style.display 	= 'flex';
     home.style.display 		= 'none';
     contact.style.display 	= 'none';
 
-    projectbtn.style.borderBottom 	= '1px solid #56535D';
-    homebtn.style.borderBottom 		= '1px solid #ddd';
-    contactbtn.style.borderBottom 	= '1px solid #ddd';
-});
+    project_btn.style.borderBottom 	= '1px solid #56535D';
+    home_btn.style.borderBottom 	= '1px solid #ddd';
+    contact_btn.style.borderBottom 	= '1px solid #ddd';
 
-homebtn.addEventListener('pointerdown', function() {
+});
+home_btn.addEventListener('pointerdown', function() {
     home.style.display 		= 'block';
     project.style.display 	= 'none';
     contact.style.display 	= 'none';
 
-    projectbtn.style.borderBottom 	= '1px solid #ddd';
-    homebtn.style.borderBottom 		= '1px solid #56535D';
-    contactbtn.style.borderBottom 	= '1px solid #ddd';
+    project_btn.style.borderBottom 	= '1px solid #ddd';
+    home_btn.style.borderBottom 	= '1px solid #56535D';
+    contact_btn.style.borderBottom 	= '1px solid #ddd';
 });
-
-contactbtn.addEventListener('pointerdown', function() {
+contact_btn.addEventListener('pointerdown', function() {
     contact.style.display 	= 'block';
     home.style.display 		= 'none';
     project.style.display 	= 'none';
 
-    projectbtn.style.borderBottom 	= '1px solid #ddd';
-    homebtn.style.borderBottom 		= '1px solid #ddd';
-    contactbtn.style.borderBottom 	= '1px solid #56535D';
+    project_btn.style.borderBottom  = '1px solid #ddd';
+    home_btn.style.borderBottom     = '1px solid #ddd';
+    contact_btn.style.borderBottom  = '1px solid #56535D';
 });
