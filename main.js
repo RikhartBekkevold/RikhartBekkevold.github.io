f691307f3ee01705557287af367d548f75b46401
RikhartBekkevold <rikhart.bekkevold@hig.no>
2018-07-28 18:30:21 +0200

    project card redesigns

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-var project = document.getElementById('project');
-var home 	= document.getElementById('home');
-var contact = document.getElementById('contact');
+const project           =   document.getElementById('project');
+const home 	            =   document.getElementById('home');
+const contact           =   document.getElementById('contact');
+const github_buttons    =   document.querySelectorAll('.github_btn');
 
+// set initial state of navigation menu
 home.style.display 				= 'none';
 contact.style.display 			= 'none';
 project_btn.style.borderBottom 	= '1px solid #56535D';
 
+// add listeners for navigation menu
 project_btn.addEventListener('pointerdown', function() {
     project.style.display 	= 'flex';
     home.style.display 		= 'none';
@@ -14,8 +17,8 @@ project_btn.addEventListener('pointerdown', function() {
     project_btn.style.borderBottom 	= '1px solid #56535D';
     home_btn.style.borderBottom 	= '1px solid #ddd';
     contact_btn.style.borderBottom 	= '1px solid #ddd';
-
 });
+
 home_btn.addEventListener('pointerdown', function() {
     home.style.display 		= 'block';
     project.style.display 	= 'none';
@@ -25,6 +28,7 @@ home_btn.addEventListener('pointerdown', function() {
     home_btn.style.borderBottom 	= '1px solid #56535D';
     contact_btn.style.borderBottom 	= '1px solid #ddd';
 });
+
 contact_btn.addEventListener('pointerdown', function() {
     contact.style.display 	= 'block';
     home.style.display 		= 'none';
@@ -33,4 +37,11 @@ contact_btn.addEventListener('pointerdown', function() {
     project_btn.style.borderBottom  = '1px solid #ddd';
     home_btn.style.borderBottom     = '1px solid #ddd';
     contact_btn.style.borderBottom  = '1px solid #56535D';
+});
+
+// add "loading" feedback for when user clicks github links
+github_buttons.forEach(function(btn) {
+    btn.addEventListener('pointerdown', function() {
+        btn.innerHTML = '<i style="margin-right: 5px;" class="fa fa-spinner fa-spin"></i>Wait..';
+    });
 });
